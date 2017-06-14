ae0f394f74757d25987099e3fe62d3bc3cf9415f
Andrew Cunningham <andrew.cunningham@saltdna.com>
2017-06-14 15:46:03 +0100

    Added more strings and translated UIViews

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,19 +12,28 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 0
     var targetValue:  Int = 0
-    var roundValue:   Int = 1
+    var levelValue:   Int = 1
     var scoreValue:   Int = 0
     
+    @IBOutlet weak var gameDesc:        UILabel!
+    @IBOutlet weak var scoreTextLabel:  UILabel!
+    @IBOutlet weak var levelTextLabeL:  UILabel!
     @IBOutlet weak var slider:          UISlider!
     @IBOutlet weak var randomNumLabel:  UILabel!
     @IBOutlet weak var scoreLabel:      UILabel!
-    @IBOutlet weak var roundLabel:      UILabel!
+    @IBOutlet weak var levelLabel:      UILabel!
     @IBOutlet weak var startOverButton: UIButton!
     @IBOutlet weak var infoButton:      UIButton!
+    @IBOutlet weak var checkButton:     UIButton!
 
     // MARK: Lifecycle Funcs
     override func viewDidLoad() {
         super.viewDidLoad()
+        startOverButton.setTitle("start_over".localised, for: .normal)
+        checkButton.setTitle("check".localised, for: .normal)
+        scoreTextLabel.text = "points".localised
+        levelTextLabeL.text = "level".localised
+        gameDesc.text       = "game_desc".localised
         startFreshRound()
         updateLabels()
     }
@@ -54,14 +63,17 @@ class ViewController: UIViewController {
             title = "unlucky".localised
             message.append("no_points".localised)
         }
-        roundValue += 1
+        levelValue += 1
         showNextLevelAlert(title: title, message: message)
     }
     
     @IBAction func showInfoAlert(){
         let rules = "rules_desc".localised
-        let alert = UIAlertController(title: "rules_title".localised, message: rules, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        let alert = UIAlertController(title: "rules_title".localised,
+                                      message: rules, preferredStyle: .alert)
+        let action = UIAlertAction(title: "dismiss".localised,
+                                   style: .default,
+                                   handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
@@ -72,27 +84,27 @@ class ViewController: UIViewController {
     }
     
     // MARK: Funcs
+    func startFreshRound(){
+        startNewRound()
+        scoreValue = 0
+        levelValue = 1
+    }
+    
     func startNewRound(){
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
     }
     
-    func startFreshRound(){
-        startNewRound()
-        scoreValue = 0
-        roundValue = 1
-    }
-    
     func updateLabels(){
         randomNumLabel.text = "\(targetValue)"
         scoreLabel.text = "\(scoreValue)"
-        roundLabel.text = "\(roundValue)"
+        levelLabel.text = "\(levelValue)"
     }
     
     func showNextLevelAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Next round", style: .default, handler: nil)
+        let action = UIAlertAction(title: "next_level".localised, style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
         startNewRound()
